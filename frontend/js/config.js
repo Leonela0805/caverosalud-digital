1234cd5df628d9284328cbdff1fbd42ab98d38bb
LeonelaQuispeCcahuana <121266449+Leonela0805@users.noreply.github.com>
2025-11-16 04:24:37 -0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,11 +1,11 @@
 // Configuración de la aplicación
 const CONFIG = {
     // La URL del backend se actualizará después de desplegar en Render
-    API_URL: 'https://tu-app-en-render.onrender.com/api',
+    API_URL: 'https://caverosalud-digital-backend.onrender.com/api',
     // Configuración de Supabase
     SUPABASE: {
-        URL: 'https://tu-proyecto-supabase.supabase.co',
-        KEY: 'tu-api-key-supabase'
+        URL: 'https://hxosnhieeqlhdmjbjoku.supabase.co',
+        KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4b3NuaGllZXFsaGRtamJqb2t1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyNTIxNDksImV4cCI6MjA3ODgyODE0OX0.EkGSPyVDhMi2FEEIGsc90_P7yQWYk409lgPOUNmG-pc'
     }
 };
 
